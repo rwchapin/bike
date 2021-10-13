431f3e223bf954b29a6ee4e2765375a3d3347d06
Ryan Chapin <ryanwchapin@yahoo.com>
2021-10-13 17:16:07 -0500

    Second commit after build out

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,6 +1,8 @@
 package com.globomatics.bike.controllers;
 
 import com.globomatics.bike.models.Bike;
+import com.globomatics.bike.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,20 +12,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
+
+    @Autowired
+    private BikeRepository bikeRepository;
+
+
     @GetMapping
     public List<Bike> list() {
-        List<Bike> bikes = new ArrayList<Bike>();
-        return bikes;
+        return bikeRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike) {
-
+        bikeRepository.save(bike);
     }
 
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
-        return new Bike();
+        return bikeRepository.getById(id);
     }
 }
--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -1,16 +1,32 @@
 package com.globomatics.bike.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.sql.Date;
 
-
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String email;
     private String phone;
     private String model;
     private String serialNumber;
     private BigDecimal purchasePrice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
     private Date purchaseDate;
     private boolean contact;
 
@@ -76,5 +92,13 @@ public class Bike {
 
     public void setContact(boolean contact) {
         this.contact = contact;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
